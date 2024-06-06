c58cdc9ddedafd844e4d3f736d00b68193742444
iravinderr <imravinderr@gmail.com>
2024-06-06 21:42:40 +0530

    backend api strings added

---
+++ frontend/src/assets/apis.js
@@ -0,0 +1,28 @@
+export const backend_host = `http://localhost:4000`;
+export const api_version = '/api/v1';
+export const base_url = `${backend_host}${api_version}/user`;
+
+
+// USER APIs
+export const sendRegisterOtpAPI = `${base_url}/register`;
+export const confirmRegistrationAPI = `${base_url}/confirm-registration`;
+export const loginAPI = `${base_url}/login`;
+export const logoutAPI = `${base_url}/logout`;
+export const verifyTokenAPI = `${base_url}/verify-token`;
+export const refreshTokensAPI = `${base_url}/refresh-tokens`;
+export const changePasswordAPI = `${base_url}/change-password`;
+export const sendResetPasswordOtpAPI = `${base_url}/send-reset-password-otp`;
+export const validateResetPasswordOtpAPI = `${base_url}/validate-reset-password-otp`;
+export const resetPasswordAPI = `${base_url}/reset-password`;
+
+// FOLDER APIs
+export const createFolderAPI = `${base_url}/folder/create`;
+export const getFoldersAPI = `${base_url}/folder/get`;
+export const renameFolderAPI = `${base_url}/folder/rename`;
+export const deleteFolderAPI = `${base_url}/folder/delete`;
+
+// FILE APIs
+export const uploadFileAPI = `${base_url}/file/upload`;
+export const getFilesAPI = `${base_url}/file/get`;
+export const renameFileAPI = `${base_url}/file/rename`;
+export const deleteFileAPI = `${base_url}/file/delete`;
