d32535be8d4501a574048cf822c0b5fb3db79db4
iravinderr <imravinderr@gmail.com>
2024-06-27 22:29:15 +0530

    available and total storage display added to the frontend

--- frontend/src/components/Header/StorageInfo.jsx
+++ frontend/src/components/Header/StorageInfo.jsx
@@ -1,30 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Pie } from "react-chartjs-2";
 import { getRequestAxios } from "../../services/requests";
 import { getStorageInfoAPI } from "../../services/apis";
 import toast from "react-hot-toast";
 
 export default function StorageInfo() {
   const [storageInfo, setStorageInfo] = useState(null);
-  const [data, setData] = useState(null);
 
   useEffect(() => {
     (async () => {
       try {
         const response = await getRequestAxios(getStorageInfoAPI);
-        console.log("response -> ", response);
         if (response.data.success) {
-          const storageInfo = response.data.data;
-          setStorageInfo(storageInfo);
-          setData({
-            labels: ['Used Storage', 'Available Storage'],
-            datasets: [{
-              label: 'Storage Info',
-              data: [storageInfo.totalStorage - storageInfo.availableStorage, storageInfo.availableStorage],
-              backgroundColor: ['#FF6384', '#36A2EB'],
-              hoverBackgroundColor: ['#FF6384', '#36A2EB']
-            }]
-          });
+          setStorageInfo(response.data.data);
         }
       } catch (error) {
         toast.success(error.response.data.message);
