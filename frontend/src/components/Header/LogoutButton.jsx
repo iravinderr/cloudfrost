358fb8923151b6e0504cefd56ece1a9415606752
iravinderr <imravinderr@gmail.com>
2024-06-27 16:47:36 +0530

    logout loading added

--- frontend/src/components/Header/LogoutButton.jsx
+++ frontend/src/components/Header/LogoutButton.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { logoutAPI } from '../../services/apis';
 import useAuthNavigation from '../../hooks/AuthNavigation';
 import toast from 'react-hot-toast';
+import Loader from '../Loader';
 
 function LogoutButton() {
   const { setAuthenticated } = useAuthNavigation();
+  const [loading, setLoading] = useState(null);
   const navigate = useNavigate();
 
   const handleLogout = async () => {
+    setLoading(true);
     try {
       const response = await axios.post(logoutAPI, null, {
         withCredentials: true,
       });
       if (response.data.success) {
+        setLoading(false);
         setAuthenticated(false);
         navigate('/');
         toast.success(response.data.message);
@@ -26,6 +30,10 @@ function LogoutButton() {
       console.error(error);
     }
   };
+
+  if (loading) {
+    return <Loader />
+  }
 
   return (
     <button
